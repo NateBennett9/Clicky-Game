951312207503b63772d50d62051551cc81bd41e2
Nate Bennett <26212562+NateBennett9@users.noreply.github.com>
2019-08-08 12:46:48 -0700

    deleted unused files, added basic header

---
+++ my-app/src/components/Game/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Header from "../Header";
+import data from "../../data.json";
+
+class Game extends Component {
+
+    render() {
+        return (
+            <div>
+                <Header />
+            </div>
+        );
+    }
+}
+
+export default Game;
