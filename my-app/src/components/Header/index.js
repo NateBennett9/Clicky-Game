c2017e2b96d9ce425a2bdcffbdddf1365ffaa976
Nate Bennett <26212562+NateBennett9@users.noreply.github.com>
2019-08-08 17:17:47 -0700

    updated css

--- my-app/src/components/Header/index.js
+++ my-app/src/components/Header/index.js
@@ -4,8 +4,9 @@ import "./style.css";
 function Header() {
   return (
     <header className="header">
-      <h1>Clicky Game!</h1>
-      <h2>Click on an image to earn points, but don't click on any more than once!</h2>
+      <h1></h1>
+      <h2>Clicky Game!</h2>
+      <h3>Click on an image to earn points, but don't click on any more than once!</h3>
     </header>
   );
 }
